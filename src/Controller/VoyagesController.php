5d66225cfb84d8c5f86a05377092f21beb4315e0
Wassim <wassimzizo06@gmail.com>
2024-09-30 19:55:18 +0200

    Phase 10 : ORM (affichage détaillé d'une visite)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -50,4 +50,10 @@ class VoyagesController extends AbstractController {
         $visites = $this->repository->findByEqualValue($champ, $valeur);
         return $this->render("pages/voyages.html.twig", ['visites' => $visites]);
     }
+    
+    #[Route('/voyages/voyage/{id}', name: 'voyages.showone')]
+    public function showOne($id): Response{
+        $visite = $this->repository->find($id);
+        return $this->render("pages/voyage.html.twig", ['visite' => $visite]);
+    }
 }
